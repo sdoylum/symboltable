93132b4bc64e06e2ad816b6c7d008617e416a82d
sdoylum <60060926+sdoylum@users.noreply.github.com>
2020-12-25 00:53:46 +0300

    Add files via upload

--- symbolTable.c
+++ symbolTable.c
@@ -2,8 +2,21 @@
 
 
 void *putsymI (symbolTable *table, char *name, int value){
+  struct symbolTable *ptr;
+  ptr = (struct symbolTable*) malloc(sizeof(struct symbolTable));
+  ptr->valueName=(char *)malloc(strlen(name)+1);
+  strcpy(ptr->valueName, name);
+  ptr->valueI = (int*)malloc(sizeof(int));
+  ptr->valueI = value;
+  if(table == 0){
 
-};
+	table=ptr;
+	return;
+  }
+
+  table->next = ptr; 
+  table = table->next;
+}
 
 symbolTable *getsym (symbolTable *table, char *name){
 	struct symbolTable *iter = table;
